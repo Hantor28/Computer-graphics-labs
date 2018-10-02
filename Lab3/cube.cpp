2ecf64d707779fd837a49c1ea51cd87e822e8ebb
Hantor28 <35535429+Hantor28@users.noreply.github.com>
2018-10-02 20:52:03 +0700

    Create cube.cpp

---
+++ Lab3/cube.cpp
@@ -0,0 +1,146 @@
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+
+void display();
+void specialKeys();
+ 
+double rotate_y=0; 
+double rotate_x=0;
+double spin = 0; 
+ void spinDisplay(void)
+{
+    spin = spin + 0.02;
+    if (spin > 360.0)
+    spin = spin - 360.0;
+    glutPostRedisplay();
+}
+void mouse(int button, int state, int x, int y) 
+{
+    switch (button) {
+    case GLUT_LEFT_BUTTON:
+    if (state == GLUT_DOWN)
+    glutIdleFunc(spinDisplay);
+    break;
+    case GLUT_MIDDLE_BUTTON:
+    if (state == GLUT_DOWN)
+    glutIdleFunc(NULL);
+    break;
+    default:
+    break;
+    }
+}
+
+
+void display(){
+
+  glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+
+  glLoadIdentity();
+ 
+  
+ 
+  // Rotate when user changes rotate_x and rotate_y
+  glRotatef( rotate_x, 1.0, 0.0, 0.0 );
+  glRotatef( rotate_y, 0.0, 1.0, 0.0 );
+ 
+  glRotatef( spin, 1.0, 1.0, 0.0 ); 
+ 
+  //Multi-colored side - FRONT
+  glBegin(GL_POLYGON);
+ 
+  glColor3f( 1.0, 0.0, 0.0 );     glVertex3f(  0.5, -0.5, -0.5 );      // P1 is red
+  glColor3f( 0.0, 1.0, 0.0 );     glVertex3f(  0.5,  0.5, -0.5 );      // P2 is green
+  glColor3f( 0.0, 0.0, 1.0 );     glVertex3f( -0.5,  0.5, -0.5 );      // P3 is blue
+  glColor3f( 1.0, 0.0, 1.0 );     glVertex3f( -0.5, -0.5, -0.5 );      // P4 is purple
+ 
+  glEnd();
+ 
+  // White side - BACK
+  glBegin(GL_POLYGON);
+  glColor3f(   1.0,  1.0, 1.0 );
+  glVertex3f(  0.5, -0.5, 0.5 );
+  glVertex3f(  0.5,  0.5, 0.5 );
+  glVertex3f( -0.5,  0.5, 0.5 );
+  glVertex3f( -0.5, -0.5, 0.5 );
+  glEnd();
+ 
+  // Purple side - RIGHT
+  glBegin(GL_POLYGON);
+  glColor3f(  1.0,  0.0,  1.0 );
+  glVertex3f( 0.5, -0.5, -0.5 );
+  glVertex3f( 0.5,  0.5, -0.5 );
+  glVertex3f( 0.5,  0.5,  0.5 );
+  glVertex3f( 0.5, -0.5,  0.5 );
+  glEnd();
+ 
+  // Green side - LEFT
+  glBegin(GL_POLYGON);
+  glColor3f(   0.0,  1.0,  0.0 );
+  glVertex3f( -0.5, -0.5,  0.5 );
+  glVertex3f( -0.5,  0.5,  0.5 );
+  glVertex3f( -0.5,  0.5, -0.5 );
+  glVertex3f( -0.5, -0.5, -0.5 );
+  glEnd();
+ 
+  // Blue side - TOP
+  glBegin(GL_POLYGON);
+  glColor3f(   0.0,  0.0,  1.0 );
+  glVertex3f(  0.5,  0.5,  0.5 );
+  glVertex3f(  0.5,  0.5, -0.5 );
+  glVertex3f( -0.5,  0.5, -0.5 );
+  glVertex3f( -0.5,  0.5,  0.5 );
+  glEnd();
+ 
+  // Red side - BOTTOM
+  glBegin(GL_POLYGON);
+  glColor3f(   1.0,  0.0,  0.0 );
+  glVertex3f(  0.5, -0.5, -0.5 );
+  glVertex3f(  0.5, -0.5,  0.5 );
+  glVertex3f( -0.5, -0.5,  0.5 );
+  glVertex3f( -0.5, -0.5, -0.5 );
+  glEnd();
+ 
+  glFlush();
+  glutSwapBuffers();
+ 
+}
+ 
+void specialKeys( int key, int x, int y ) {
+ 
+  if (key == GLUT_KEY_RIGHT)
+    rotate_y += 5;
+
+  else if (key == GLUT_KEY_LEFT)
+    rotate_y -= 5;
+ 
+  else if (key == GLUT_KEY_UP)
+    rotate_x += 5;
+ 
+  else if (key == GLUT_KEY_DOWN)
+    rotate_x -= 5;
+
+  glutPostRedisplay();
+ 
+}
+
+
+int main(int argc, char* argv[]){
+ 
+  glutInit(&argc,argv);
+ 
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+ 
+  glutCreateWindow("Awesome Cube");
+ 
+  glEnable(GL_DEPTH_TEST);
+ 
+  glutDisplayFunc(display);
+  glutSpecialFunc(specialKeys);
+  glutMouseFunc(mouse);
+ 
+  glutMainLoop();
+ 
+  return 0;
+ 
+}
